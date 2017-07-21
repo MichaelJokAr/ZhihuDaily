a13b7822ebe8b54dafe74e595811b1725ca97c6d
JokAr <1018875550@qq.com>
2017-07-21 16:04:48 +0800

    ing

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ ZhihuDaily for Kotlin,[说明请看这里](http://www.jianshu.com/p/1114c471281b
 - Java version: 1.8
 - Kotlin version: 1.1.3
 - SDK Tool: 25.0.2
+- [知乎日报API](https://github.com/izzyleung/ZhihuDailyPurify/wiki/%E7%9F%A5%E4%B9%8E%E6%97%A5%E6%8A%A5-API-%E5%88%86%E6%9E%90)
 
 
-### Points
+### *
 - MVP
 - [dagger2(v2.11)](https://google.github.io/dagger/)
 - rx
--- app/src/test/java/com/github/jokar/zhihudaily/di/module/network/NewsModuleTest.kt
+++ app/src/test/java/com/github/jokar/zhihudaily/di/module/network/NewsModuleTest.kt
@@ -1,10 +1,16 @@
 package com.github.jokar.zhihudaily.di.module.network
 
 import android.util.Log
-import com.github.jokar.zhihudaily.BuildConfig
-import com.github.jokar.zhihudaily.di.module.NetworkModule
 import com.github.jokar.zhihudaily.model.entities.story.StoryDetail
 import com.sunagy.mazcloud.utlis.rxjava.SchedulersUtil
+import io.reactivex.Scheduler
+import io.reactivex.functions.Function
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import okhttp3.CipherSuite
+import okhttp3.ConnectionSpec
+import okhttp3.OkHttpClient
+import okhttp3.TlsVersion
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,10 +21,10 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowLog
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.CountDownLatch
-import android.security.NetworkSecurityPolicy
-import org.robolectric.annotation.Implementation
-import org.robolectric.annotation.Implements
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -27,6 +33,8 @@ import org.robolectric.annotation.Implements
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = intArrayOf(23))
 class NewsModuleTest {
+    val DEFAULT_TIMEOUT = 15
+    val BASE_URL = "http://news-at.zhihu.com/api/4/"
 
     lateinit var retrofit: Retrofit
     @Mock
@@ -37,30 +45,42 @@ class NewsModuleTest {
         ShadowLog.stream = System.out
         //初始化Mock
         MockitoAnnotations.initMocks(this)
+        //init Retrofit
 
-        retrofit = NetworkModule().providerRetrofit()
+        val client = OkHttpClient.Builder()
+                .retryOnConnectionFailure(true)
+                .connectTimeout(DEFAULT_TIMEOUT.toLong(), TimeUnit.SECONDS)
+                .readTimeout(DEFAULT_TIMEOUT.toLong(), TimeUnit.SECONDS)
+                .build()
+
+        retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        RxJavaPlugins.setIoSchedulerHandler(Function<Scheduler, Scheduler> {
+            Schedulers.single()
+        })
+
 
     }
 
     @Test
     fun provider() {
-        val services  = Mockito.verify(news).provider(retrofit).getNews(3892357)
-        var latch = CountDownLatch(1)
+        val services = Mockito.verify(news).provider(retrofit).getNews(3892357)
+        val latch = CountDownLatch(1)
         services.compose(SchedulersUtil.applySchedulersIO())
                 .subscribe({
                     t: StoryDetail ->
-                    Log.w("test", t.toString())
+                    Log.d("test", t.toString())
                     latch.countDown()
                 }, {
-                    error ->
-                    error.printStackTrace()
+                    e ->
+                    e.printStackTrace()
                     latch.countDown()
-                }, {
-
-                }, {
-                    Log.d("test", "onSatrt")
                 })
-
         latch.await()
     }
 
